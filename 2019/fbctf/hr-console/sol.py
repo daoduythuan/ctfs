625c00f6e4598a78cf6e32d10a9ca74463897930
Dao Duy Thuan <daoduythuan.IT@gmail.com>
2019-06-07 11:38:56 +0700

    Update sol.py

--- 2019/fbctf/hr-console/sol.py
+++ 2019/fbctf/hr-console/sol.py
@@ -1,1 +1,2 @@
-[a]https://github.com/PDKT-Team/ctf/blob/master/fbctf2019/hr-admin-module/README.md
+url = 'https://github.com/PDKT-Team/ctf/blob/master/fbctf2019/hr-admin-module/README.md'
+print 'hr_admin_module'
